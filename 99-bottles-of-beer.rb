405d3b4df007194df87a7c84897ae1486e55cba6
David Montesdeoca <backpackerhh@gmail.com>
2019-02-23 11:24:51 +0100

    Add first iteration with correct message and lot of duplication

--- 99-bottles-of-beer.rb
+++ 99-bottles-of-beer.rb
@@ -1,4 +1,15 @@
 99.downto(1) do |number|
-  puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
-  puts "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n\n"
+  if number == 1
+    puts "1 bottle of beer on the wall, 1 bottle of beer."
+    puts "Take one down and pass it around, no more bottles of beer on the wall."
+  elsif number == 2
+    puts "2 bottles of beer on the wall, 2 bottles of beer."
+    puts "Take one down and pass it around, 1 bottle of beer on the wall."
+  else
+    puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+    puts "Take one down and pass it around, #{number - 1} bottles of beer on the wall."
+  end
 end
+
+puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
