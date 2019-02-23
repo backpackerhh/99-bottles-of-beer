fa32ce990304a347ee6b527801a640afc4e0e51a
David Montesdeoca <backpackerhh@gmail.com>
2019-02-23 23:08:16 +0100

    Refactor code using an OOP approach

--- 99-bottles-of-beer.rb
+++ 99-bottles-of-beer.rb
@@ -1,20 +1,80 @@
-def bottles_of_beer(number, pluralize_unless = 1)
-  "bottle#{'s' if number != pluralize_unless} of beer"
-end
+class BottlesSong
+  def self.build
+    99.downto(0) do |number|
+      builder = builder_for(number)
 
-99.downto(0) do |number|
-  current_bottles = number
-  action = 'Take one down and pass it around'
-  remaining_bottles = number - 1
-
-  if number == 0
-    current_bottles = 'no more'
-    action = 'Go to the store and buy some more'
-    remaining_bottles = 99
-  elsif number == 1
-    remaining_bottles = 'no more'
+      puts "#{builder.current_bottles.capitalize} of beer on the wall, #{builder.current_bottles} of beer."
+      puts "#{builder.action}, #{builder.remaining_bottles} of beer on the wall.\n\n"
+    end
   end
 
-  puts "#{current_bottles.to_s.capitalize} #{bottles_of_beer(number)} on the wall, #{current_bottles} #{bottles_of_beer(number)}."
-  puts "#{action}, #{remaining_bottles} #{bottles_of_beer(number, 2)} on the wall.\n\n"
+  private_class_method def self.builder_for(number)
+    klass = case number
+            when 0
+              NoBottles
+            when 1
+              OneBottle
+            when 2
+              TwoBottles
+            else
+              MultipleBottles
+            end
+
+    klass.new(number)
+  end
+
+  class BottleTemplate
+    attr_reader :number
+
+    def initialize(number)
+      @number = number
+    end
+
+    def current_bottles
+      "#{number} bottles"
+    end
+
+    def action
+      'Take one down and pass it around'
+    end
+
+    def remaining_bottles
+      "#{number - 1} bottles"
+    end
+  end
+
+  class NoBottles < BottleTemplate
+    def current_bottles
+      'no more bottles'
+    end
+
+    def action
+      'Go to the store and buy some more'
+    end
+
+    def remaining_bottles
+      '99 bottles'
+    end
+  end
+
+  class OneBottle < BottleTemplate
+    def current_bottles
+      '1 bottle'
+    end
+
+    def remaining_bottles
+      'no more bottles'
+    end
+  end
+
+  class TwoBottles < BottleTemplate
+    def remaining_bottles
+      '1 bottle'
+    end
+  end
+
+  class MultipleBottles < BottleTemplate
+  end
 end
+
+BottlesSong.build
