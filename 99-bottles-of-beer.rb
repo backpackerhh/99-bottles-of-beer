103c65e7b2b11cfe42822b763a18ea72b61a5064
David Montesdeoca <backpackerhh@gmail.com>
2019-02-23 11:20:29 +0100

    Add first iteration with wrong message
    Lyrics:
    http://www.99-bottles-of-beer.net/lyrics.html

---
+++ 99-bottles-of-beer.rb
@@ -0,0 +1,4 @@
+99.downto(1) do |number|
+  puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+  puts "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n\n"
+end
