6804397e9ce965dd34e3367c55737b38e094f9bb
David Montesdeoca <backpackerhh@gmail.com>
2019-02-23 12:19:12 +0100

    Add another refactor to get rid of some conditionals

--- 99-bottles-of-beer.rb
+++ 99-bottles-of-beer.rb
@@ -1,23 +1,16 @@
 99.downto(0) do |number|
-  current_bottles = if number == 0
-                      'no more'
-                    else
-                      number
-                    end
-  action = if number == 0
-             'Go to the store and buy some more'
-           else
-             'Take one down and pass it around'
-           end
-  remaining_bottles = if number == 0
-                        99
-                      elsif number == 1
-                        'no more'
-                      else
-                        number - 1
-                      end
+  current_bottles = number
+  action = 'Take one down and pass it around'
+  remaining_bottles = number - 1
+
+  if number == 0
+    current_bottles = 'no more'
+    action = 'Go to the store and buy some more'
+    remaining_bottles = 99
+  elsif number == 1
+    remaining_bottles = 'no more'
+  end
 
   puts "#{current_bottles.to_s.capitalize} bottle#{'s' if number != 1} of beer on the wall, #{current_bottles} bottle#{'s' if number != 1} of beer."
   puts "#{action}, #{remaining_bottles} bottle#{'s' if number != 2} of beer on the wall.\n\n"
 end
-
